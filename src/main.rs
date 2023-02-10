22620c69dfb401520ac7fc141f237a0131ec96d9
Estefan Hu <ehu@goodwaygroup.com>
2023-02-09 20:12:35 -0800

    added db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().expect("Key was not found");
@@ -5,4 +7,30 @@ fn main() {
     println!("The key is '{}' and the value is '{}'", key, value);
     let contents = format!("{}\t{}\n", key, value);
     std::fs::write("kv.db", contents).unwrap();
+
+    let database = Database::new().expect("Database::new() crashed");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        // read kv.db file
+        // let contents = match std::fs::read_to_string("kv.db") {
+        //     Ok(c) => c,
+        //     Err(error) => {
+        //         return Err(error);
+        //     }
+        // };
+        let contents = std::fs::read_to_string("kv.db")?;
+        // parse the string
+
+        // populate our map
+
+        Ok(Database {
+            map: HashMap::new(),
+        })
+    }
 }
